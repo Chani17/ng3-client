7c82dfd331f163895d07705a0f94694b1f7b52b2
HyeokJinPark <phj970701@gmail.com>
2024-08-27 03:05:26 +0900

    FEAT: 404페이지 구현
    -잘못된 URL주소 입력 시 router를 이용해 404페이지로 가는 기능 구현

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from 'vue-router';
 import LoginView from '@/views/LoginView.vue';
 import MainPage from '@/views/MainPage.vue';
 import GamePage from '@/views/GamePage.vue';
+import NotFoundView from '@/components/common/NotFoundView.vue';
 
 Vue.use(VueRouter);
 
@@ -24,6 +25,10 @@ export const router = new VueRouter({
     {
       path: '/game/:roomId',
       component: GamePage,
+    },
+    {
+      path: '*',
+      component: NotFoundView, // 이상한 주소를 입력했을때 와일드카드 경로로 404 페이지 연결
     },
   ],
 });
