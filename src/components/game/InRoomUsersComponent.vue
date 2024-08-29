c87fc7d5c62366829c778709293b958929aeb4b1
Halyn-lee <leehalyn@gmail.com>
2024-08-29 21:14:29 +0900

    Feat : 게임페이지 UI 초기 작업2

--- src/components/game/InRoomUsersComponent.vue
+++ src/components/game/InRoomUsersComponent.vue
@@ -1,11 +1,14 @@
 <template>
   <div class="inRoomUsersContainer">
     <div v-for="user in users" :key="user.id" class="user">
-      <!-- <div>{{ user.email }}</div> pk 가져와집니다 -->
-      <div class="userInfoGroupContainer">
+      <!-- <div>{{ user.email }}</div> pk 가져와짐 -->
+      <div class="userInfoGroupContainer1">
         <img :src="user.profile_image" style="width: 50px; height: 50px" />
-        {{ user.nickname }}
-        <button>follow</button>
+        <div class="userInfoGroupContainer2">
+          <div>3 point</div> <!-- 실제 값으로 렌더링해야함 -->
+          {{ user.nickname }}
+          <button>follow</button>
+        </div>
       </div>
     </div>
   </div>
@@ -36,9 +39,16 @@ export default {
 .user {
   margin: 5px 0;
 }
-.userInfoGroupContainer {
-    border: 1px solid black;
-    height: 65px;
-    width: 180px;
+.userInfoGroupContainer1 {
+  border: 1px solid black;
+  height: 65px;
+  width: 180px;
+  display: flex;
+}
+
+.userInfoGroupContainer2 {
+    margin-left: 5px;
+  margin-top: 3px;
+  flex-direction: row;
 }
 </style>
