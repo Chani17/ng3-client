98545e23ce27d21c6b0dae2353ee88048a4bdf24
Halyn-lee <leehalyn@gmail.com>
2024-08-29 10:26:03 +0900

    Style : 메인페이지 헤더 내 로고 추가

--- src/components/main/HeaderComponent.vue
+++ src/components/main/HeaderComponent.vue
@@ -1,9 +1,14 @@
 <template>
   <div class="headerContainer">
     <div class="router-box">
-      <router-link to="#">갤러리 | </router-link>
-      <router-link to="#">팔로우 | </router-link>
-      <router-link to="#">마이페이지</router-link>
+      <div class="homeLogoContainer">
+        <img src="@/assets/image/home.png" link="#" class="homeLogo">
+      </div>
+      <div class="linkContainer">
+        <router-link to="#">갤러리 | </router-link>
+        <router-link to="#">팔로우 | </router-link>
+        <router-link to="#">마이페이지</router-link>
+      </div>
     </div>
   </div>
 </template>
@@ -17,15 +22,35 @@ export default {};
   border: none;
   margin: 0 60px;
   margin-top: 180px;
-  margin-bottom: 20px;
+  margin-bottom: -20px;
 }
 
 .router-box {
   display: flex;
-  justify-content: end;
+  justify-content: space-between;
+  align-items: center;
+  margin: 0 52px;
+}
+
+.homeLogoContainer {
+  flex: 1;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+}
+
+.homeLogo {
+  width: 70px;
+  height: 70px;
+  cursor: pointer;
+}
+
+.linkContainer {
+  display: flex;
+  justify-content: flex-end;
   align-items: center;
   gap: 10px;
-  margin: 0 52px;
+  flex: 1;
 }
 
 a {
